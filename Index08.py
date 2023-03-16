5be47573c6ae94cf6d819cc0f7e2d7346b1d9e3c
husan999 <husanshunqorov07@gmail.com>
2023-03-16 06:56:26 +0500

    good

--- Index08.py
+++ Index08.py
@@ -6,5 +6,10 @@ def main(s):
     Returns:
         int: answer
     """
-    return
+    if len(s)==4:
+        return s.index('d')
+    else:
+        return False
+print(main("good"))
+
         
