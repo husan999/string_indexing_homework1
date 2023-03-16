d5597fa227b70d946af8e73d821aaf8d6bf0ff2a
husan999 <husanshunqorov07@gmail.com>
2023-03-16 07:17:25 +0500

    good

--- Index10.py
+++ Index10.py
@@ -6,4 +6,11 @@ def main(s):
     Returns:
         int: answer
     """
-    return
+    x1=int(s)//10000
+    x2=int(s)//1000%10
+    x3=int(s)//100%10
+    x4=int(s)//10%10
+    x5=int(s)%10
+    x=x1+x2+x3+x4+x5
+    return x
+print(main("12332"))
